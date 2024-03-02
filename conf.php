eb449e8bcde8131faada1a3ea73a89f2caa3317f
3Soroush <65729233+3Soroush@users.noreply.github.com>
2024-03-02 11:57:18 +0330

    Update conf.php

--- conf.php
+++ conf.php
@@ -4,7 +4,7 @@ $conf = [
 	//Change to your bot token, check the readme to know how to create and get your bot token.
 	'bot_token' => '6899981488:AAFkTqeaU5fIQ-Q-Gy5m3uRX9w5EViwnFy0',
 	//Set to TRUE if you want the bot only to awnser the trusted ChatIDs
-	'only_trusted' => TRUE,
+	'only_trusted' => FALSE,
 	//Populate the array with the trusted Chat IDs
 	'trusted' => [ 
 		'779918878' 
